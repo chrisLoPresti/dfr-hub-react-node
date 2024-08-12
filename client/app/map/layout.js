06ce7112d3c6ce8cc40e3b4a0ce80b8e2f76253f
Christopher LoPresti <lopresti.chris@yahoo.com>
2024-08-12 12:54:57 -0400

    update markers entirely

--- client/app/map/layout.js
+++ client/app/map/layout.js
@@ -18,15 +18,13 @@ export default function MapLayout({ children }) {
   return (
     <SocketContextProvider>
       <MapContextProvider>
-        {user && (
-          <header className="sticky top-0 z-20 h-16 bg-slate-900">
-            <div className="flex items-center w-full h-full">
-              <p className="text-white ml-auto mr-2.5">
-                {user.first_name} {user.last_name}
-              </p>
-            </div>
-          </header>
-        )}
+        <header className="sticky top-0 z-20 h-16 bg-slate-900">
+          <div className="flex items-center w-full h-full">
+            <p className="text-white ml-auto mr-2.5">
+              {user.first_name} {user.last_name}
+            </p>
+          </div>
+        </header>
         <div className="flex h-[calc(100vh_-_64px)] relative overflow-hidden bg-slate-700">
           <SideNavigation />
           {children}
